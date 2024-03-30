3319618035dcc33950dcbb2625219eb61dfd09eb
chdegen <christian.degen@icfm.ch>
2024-03-30 07:57:12 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+bool WaitForKey()
+{
+    Console.ReadKey(true);
+    return true;
+}
+
+var notes = new[] { 'F', 'C', 'G', 'D', 'A', 'E', 'B' };
+var guitarStrings = new[] { 'E', 'B', 'G', 'D', 'A' };
+var random = new Random();
+
+Console.WriteLine("Hit any key for next note or Ctrl + C to exit.");
+
+do
+{
+    var noteIndex = random.Next(0, notes.Length);
+    var guitarStringIndex = random.Next(0, guitarStrings.Length);
+    Console.WriteLine($"Play {notes[noteIndex]} on {guitarStrings[guitarStringIndex]} string.");
+} while (WaitForKey());
+
+
+
